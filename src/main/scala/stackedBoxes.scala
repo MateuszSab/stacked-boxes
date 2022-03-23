a449dfb80827f67b4ebffd5ee41e1de7be744990
m.sabatowski <ma.sabatowski@gmail.com>
2022-03-23 13:47:44 +0100

    Second commit

--- src/main/scala/stackedBoxes.scala
+++ src/main/scala/stackedBoxes.scala
@@ -1,12 +1,16 @@
 object stackedBoxes extends App {
 
   def prt(s: String): Unit = {
+    println("+" + " " * (s.length + 2) + "+")
+    println("|" + " " * (s.length + 2) + "|")
     println("+" + "-" * (s.length + 2) + "+")
     println(s"| $s |")
     println("+" + "-" * (s.length + 2) + "+")
   }
 
   def prtStack(ls: List[String]): Unit = {
+    println(" " * ((ls.last.length + 3) / 2) + "*")
+    println(" " * ((ls.last.length + 3) / 2) + "|")
     ls.foldLeft(())((m, n) => {
       println(" " * ((ls.last.length - n.length) / 2) + "+" + "-" * (n.length + 2) + "+")
       println(" " * ((ls.last.length - n.length) / 2) + s"| $n |")
@@ -14,14 +18,13 @@ object stackedBoxes extends App {
     println("+" + "-" * (ls.last.length + 2) + "+")
   }
 
-  val words = args.toList.sortBy(_.size)
+  val words = args.toList.sortBy(_.length)
 
   words match {
     case Nil => println("Nie ma torcika")
     case m :: Nil => prt(m)
-    case _ => {
-      prtStack(words)
-    }
+    case _ => prtStack(words)
   }
 
 }
+
